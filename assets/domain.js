3882dfb309590607b9472d8968ce9f9a29361ae3
Gilberto Costa <127106886+horusoni@users.noreply.github.com>
2026-02-04 11:03:17 -0300

    update_dns

--- assets/domain.js
+++ assets/domain.js
@@ -1,3 +1,3 @@
-let domain = "http://192.168.100.12:4444"
-//let domain = "https://api.newworldcursos.com.br"
+//let domain = "http://192.168.100.12:4444"
+let domain = "https://api.newworldcursos.com.br"
 
